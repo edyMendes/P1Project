e75f80e6e1e985c391e6d5e8804e12a28d60d1d6
edyMendes <edgarmendes5461@gmail.com>
2020-12-27 22:02:03 +0000

    Adicionei as funções auxiliares e comecei por fazer as estruturas

---
+++ funcoes_auxiliares.h
@@ -0,0 +1,30 @@
+#ifndef FUNCOES_AUXILIARES_H_INCLUDED
+#define FUNCOES_AUXILIARES_H_INCLUDED
+
+#define MINANO 2000
+#define MAXANO 2500
+
+#define MIN_HORA 8
+#define MAX_HORA 24
+#define MIN_MINUTO 0
+#define MAX_MINUTO 59
+
+typedef struct
+{
+    int dia, mes, ano;
+} tipoData;
+
+typedef struct
+{
+    int hora, minuto;
+} tipoHora;
+
+int lerInteiro(char mensagem[], int minimo, int maximo);
+float lerFloat(char mensagem[], float minimo, float maximo);
+void lerString(char mensagem[], char vetorCaracteres[], int maximoCaracteres);
+void limpaBufferStdin(void);
+
+tipoData lerData(char mensagem[]);
+void escreverData (tipoData data);
+
+#endif // FUNCOES_AUXILIARES_H_INCLUDED
---
+++ gestaoUCs.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+#include "gestaoUCs.h"
+#include "funcoes_auxiliares.h"
