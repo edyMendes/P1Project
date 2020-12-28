dfca814081476b49c27bd3b92fc026b74077a87e
XxN4BxX <ichi_kun.wilson@hotmail.com>
2020-12-28 10:30:20 +0000

    Add files via upload

---
+++ Main Project/gestaoUCs.h
@@ -0,0 +1,29 @@
+#ifndef GESTAOUCS_H_INCLUDED
+#define GESTAOUCS_H_INCLUDED
+
+#include "funcoes_auxiliares.h"
+
+typedef struct{
+    int codigo;
+    char designacao;
+    char tipoUc;
+    int semestre;
+    char regime;
+    int quantPrevista;
+    char tipoAula;
+    int duracao;
+} tipoUCs;
+
+typedef struct{
+    int designacao;
+    int codigoUC;
+    char tipoAula;
+    char nomeDocente;
+    tipoData data;
+    tipoHora horaInicio;
+    tipoHora horaFim;
+    char estadoAula;
+    char opcaoGravacao;
+} tipoAulaOnline;
+
+#endif // GESTAOUCS_H_INCLUDED
---
+++ Main Project/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("teste para ver o que foi alterado\n");
+    
+    printf("teste para ver o que foi adicionado\n\n ")
+    return 0;
+}
