6e0f5db79502aad1ba5c9fc00abe80e590f20379
XxN4BxX <ichi_kun.wilson@hotmail.com>
2020-12-28 11:15:01 +0000

    rebuild project with extra files

--- P1Project-main/funcoes_auxiliares.h
+++ P1Project-main/funcoes_auxiliares.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:P1Project-main/funcoes_auxiliares.h
 #ifndef FUNCOES_AUXILIARES_H_INCLUDED
 #define FUNCOES_AUXILIARES_H_INCLUDED
 
@@ -18,3 +19,38 @@ tipoData lerData(char mensagem[]);
 void escreverData (tipoData data);
 
 #endif // FUNCOES_AUXILIARES_H_INCLUDED
+=======
+#ifndef FUNCOES_AUXILIARES_H_INCLUDED
+#define FUNCOES_AUXILIARES_H_INCLUDED
+
+#define MINANO 2000
+#define MAXANO 2500
+
+#define MIN_HORA 8
+#define MAX_HORA 24
+#define MIN_MINUTO 0
+#define MAX_MINUTO 59
+
+typedef struct
+{
+    int dia, mes, ano;
+} tipoData;
+
+typedef struct
+{
+    int hora, minuto;
+} tipoHora;
+
+int lerInteiro(char mensagem[], int minimo, int maximo);
+float lerFloat(char mensagem[], float minimo, float maximo);
+void lerString(char mensagem[], char vetorCaracteres[], int maximoCaracteres);
+void limpaBufferStdin(void);
+
+tipoData lerData(char mensagem[]);
+void escreverData (tipoData data);
+
+tipoHora lerHora(char mensagem[]);
+void escreverHora (tipoHora hora);
+
+#endif // FUNCOES_AUXILIARES_H_INCLUDED
+>>>>>>> Stashed changes:Main Project/funcoes_auxiliares.h
