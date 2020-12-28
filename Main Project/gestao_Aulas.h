2178b6c34d451f8ae566358d4d74aab13662302f
XxN4BxX <ichi_kun.wilson@hotmail.com>
2020-12-28 16:51:23 +0000

    test 2

--- Main Project/gestao_Aulas.h
+++ Main Project/gestao_Aulas.h
@@ -1,6 +1,18 @@
 #ifndef GESTAO_AULAS_H_INCLUDED
 #define GESTAO_AULAS_H_INCLUDED
 
+#include "funcoes_auxiliares.h"
 
+typedef struct{
+    int designacao;
+    int codigoUC;
+    char tipoAula;
+    char nomeDocente;
+    tipoData data;
+    tipoHora horaInicio;
+    tipoHora horaFim;
+    char estadoAula;
+    char opcaoGravacao;
+} tipoAulaOnline;
 
 #endif // GESTAO_AULAS_H_INCLUDED
